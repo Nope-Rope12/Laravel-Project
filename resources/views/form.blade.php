bbefcb7d2a4b45bf97bfc5d493a991b265f80447
Neeraj01-dev <neerajkahal111@gmail.com>
2025-07-14 13:33:40 +0530

    Your commit message

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Form</title>
-  <link rel="stylesheet" href="{{ asset('assets/styling.css') }}">
+  <link rel="stylesheet" href="{{asset('assets/styling.css')}}">
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
